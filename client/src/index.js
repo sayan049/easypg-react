fd69383fd7b3e89589b97fc37cad9d403b9d2c77
tanmoy06 <iamtanmoysarkar007@gmail.com>
2024-05-14 01:41:36 +0530

    router added

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
