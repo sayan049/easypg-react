455c52858a8102ca99aa2b8d05d71f724fa49674
rishi <rishisaha302016@gmail.com>
2024-05-25 01:23:50 +0530

    error  conflict

--- client/src/pages/messFind.jsx
+++ client/src/pages/messFind.jsx
@@ -7,110 +7,119 @@ import { useState } from "react";
 import ToggleSwitch from '../components/toggle';
 import Dropdown from '../components/dropdown';
 import MessBars from '../components/messBars';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 // downlead package  google-map-react !!!!
 
 function MessFind() {
-    const [range, setRange] = useState([1000, 3000]);
-    function handleChanges(event, newValue) {
-        // Ensure minimum value is not greater than maximum value
-    if (newValue[0] > newValue[1]) {
-        newValue[0] = newValue[1];
+  const [range, setRange] = useState([1000, 3000]);
+
+  function handleChanges(event, newValue) {
+      // Ensure minimum value is not greater than maximum value
+      if (newValue[0] > newValue[1]) {
+          newValue[0] = newValue[1];
       }
       // Ensure maximum value is not smaller than minimum value
       if (newValue[1] < newValue[0]) {
-        newValue[1] = newValue[0];
+          newValue[1] = newValue[0];
       }
       setRange(newValue);
-    }
+  }
 
-    const checkboxesData = [
+  const checkboxesData = [
       { logo: 'air-conditioner 1.png', title: 'A/C' },
       { logo: 'tank-water 1.png', title: 'tank water' },
       { logo: 'wifi (1) 1.png', title: 'WiFi' },
-      { logo:'power 1.png' ,title:'Power Backup'},
-      { logo:'restaurant 1.png' ,title:'Kitchen'},
-      { logo:'single-bed 1.png' ,title:'Single Bed'},
-      { logo:'single-bed (1) 1.png' ,title:'Double Bed'},
-    ];
+      { logo: 'power 1.png', title: 'Power Backup' },
+      { logo: 'restaurant 1.png', title: 'Kitchen' },
+      { logo: 'single-bed 1.png', title: 'Single Bed' },
+      { logo: 'single-bed (1) 1.png', title: 'Double Bed' },
+  ];
 
-    const [isChecked, setIsChecked] = useState(false);
-
-    const handleToggle = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleToggle = () => {
       setIsChecked(!isChecked);
-    };
+  };
+
+  const isWideScreen = useMediaQuery('(min-width:1500px)');
+  console.log(isWideScreen);
 
   return (
-    <div style={{padding : "0px 45px ",}}>
-        <div className="leftCon inlineblock ">
-            <div className="priceFilter">
-                <div className='justifyspace flex'><p>filter price</p> <p>clear all</p></div>
-                <div className="liver">
-      <Box sx={{ width: "105%" , margin: 'auto', textAlign: 'center' }}>
-        <Typography id="range-slider" gutterBottom>
-          Select Range
-        </Typography>
-        <Slider
-          min={1000}
-          max={10000}
-          step={100}
-          value={range}
-          onChange={handleChanges}
-          valueLabelDisplay="auto"
-        />
-        <Typography variant="body2">
-          Range: {range[0]} - {range[1]}
-        </Typography>
-      </Box>
-    </div>
-             </div>
-             <div className='flex  flexcolumn'>
-              {checkboxesData.map((data,index)=>(
-                <div className="checkContainer flex  aligncentre" key={index}> <div className="checkboxicon"><img className="checkimg"   src={`${process.env.PUBLIC_URL}/assets/${data.logo}`}
-                alt="" srcset=""/></div><input type="checkbox" className="checki" id={`test${index}`} />
-                <label style={{bottom: "4px"}} for={`test${index}`} ></label><div className="checkboxtxt flex alingcentre"  >{data.title}</div></div>
-              ))}
-     
-        </div>
-        </div>
+      <div style={{ padding: "0px 45px " }}>
+          <div className="leftCon inlineblock ">
+              <div className="priceFilter">
+                  <div className='justifyspace flex'>
+                      <p>filter price</p>
+                      <p>clear all</p>
+                  </div>
+                  <div className="liver">
+                      <Box sx={{ width: "105%", margin: 'auto', textAlign: 'center' }}>
+                          <Typography id="range-slider" gutterBottom>
+                              Select Range
+                          </Typography>
+                          <Slider
+                              min={1000}
+                              max={10000}
+                              step={100}
+                              value={range}
+                              onChange={handleChanges}
+                              valueLabelDisplay="auto"
+                          />
+                          <Typography variant="body2">
+                              Range: {range[0]} - {range[1]}
+                          </Typography>
+                      </Box>
+                  </div>
+              </div>
+              <div className='flex flexcolumn'>
+                  {checkboxesData.map((data, index) => (
+                      <div className="checkContainer flex aligncentre" key={index}>
+                          <div className="checkboxicon">
+                              <img className="checkimg" src={`${process.env.PUBLIC_URL}/assets/${data.logo}`} alt="" srcSet="" />
+                          </div>
+                          <input type="checkbox" className="checki" id={`test${index}`} />
+                          <label style={{ bottom: "4px" }} htmlFor={`test${index}`} />
+                          <div className="checkboxtxt flex aligncentre">{data.title}</div>
+                      </div>
+                  ))}
+              </div>
+          </div>
 
-        <div className="rightCon inlineblock" >
-          <div className="upnav flex justifyspace">
-            <div className="dynamicmessdetails flex aligncentre justifycentre">20 Mess in Simhat, Nadia , West Bengal, India</div>
-            <div className="togglebtn flex aligncentre"> map view<ToggleSwitch isChecked={isChecked} handleToggle={handleToggle}/> </div>
-            <div className="shortby"><Dropdown/></div>
+          <div className="rightCon inlineblock">
+              <div className="upnav flex justifyspace">
+                  <div className="dynamicmessdetails flex aligncentre justifycentre">20 Mess in Simhat, Nadia, West Bengal, India</div>
+                  <div className="togglebtn flex aligncentre"> map view<ToggleSwitch isChecked={isChecked} handleToggle={handleToggle} /> </div>
+                  <div className="shortby"><Dropdown /></div>
+              </div>
+              <div className="messbody" style={{ display: isChecked ? "inline-block" : (isWideScreen ? "grid" : "block") }}>
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+                  <MessBars />
+              </div>
+              <div className="mapview" style={{ display: isChecked ? (isWideScreen ? "inline-block" : "block") : (isWideScreen ? "grid" : "block") }}>
+                  <div className="mapcontainer">
+                      {/* <GoogleMapReact
+                          bootstrapURLKeys={{ key: "YOUR_API_KEY" }}
+                          defaultCenter={location}
+                          defaultZoom={17}>
+                          <LocationPin
+                              lat={location.lat}
+                              lng={location.lng}
+                              text={location.address}
+                          />
+                      </GoogleMapReact> */}
+                      google map
+                  </div>
+              </div>
           </div>
-          <div className="messbody" style={{display: isChecked ? "inline-block" : "grid" , width: isChecked ? "50%" : "100%"}}>
-            <MessBars />
-            <MessBars />
-            <MessBars />
-            <MessBars />
-            <MessBars />
-            <MessBars />
-            <MessBars />
-            <MessBars />
-          </div>
-          <div className="mapview" style={{display: isChecked ? "inline-block" : "none" , width: isChecked ? "39%" : "100%"}}>
-           <div className="mapcontainer">
-            {/* <GoogleMapReact
-          bootstrapURLKeys={{ key: "YOUR_API_KEY" }}
-          defaultCenter={location}
-          defaultZoom={17}>
-          <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          />
-        </GoogleMapReact> */}
-        google map
-        </div>
-          </div>
-        </div>
-    </div>
-  )
+      </div>
+  );
 }
 
-export default MessFind
-/* a49c07cbf2a456aed0a594170a0e0e43 1 */
-
-
+export default MessFind;
