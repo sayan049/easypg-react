c95bc8d4080f80bc4e9e6188e2d05b7036d721d5
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-11 21:10:48 +0530

    new mess find page updated

--- client/src/components/messBars.js
+++ client/src/components/messBars.js
@@ -4,19 +4,20 @@ import { baseurl, findMessUrl } from '../constant/urls';
 import "../designs/messfind.css";
 import '../designs/messbars.css'
 
-function MessBars({ isChecked /* ,checkFeatures}*/ }) {
+function MessBars({ isChecked  ,checkFeatures} ) {
   const [messData, setMessData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+       // console.log("baby"+checkFeatures);
         const res = await axios.get(findMessUrl);
-      //   const filteredData = res.data.filter(owner =>
-      //     checkFeatures.every(feature => owner.facility[0].split(',').includes(feature))
-      // );
-     //  console.log(filteredData)
-        setMessData(res.data);
+        const filteredData = res.data.filter(owner =>
+          checkFeatures.every(feature => owner.facility?.[0]?.split(',').includes(feature))
+      );
+      console.log(filteredData)
+        setMessData(filteredData);
         
       } catch (err) {
         console.log("Error fetching data", err);
@@ -24,7 +25,7 @@ function MessBars({ isChecked /* ,checkFeatures}*/ }) {
       }
     };
     fetchData();
-  }/*, [checkFeatures]*/);
+  }, [checkFeatures]);
 
   if (error) {
     return <div>{error}</div>;
@@ -52,11 +53,12 @@ function MessBars({ isChecked /* ,checkFeatures}*/ }) {
       <div
         key={owner._id}
         className="flex flex-col md:flex-row bg-white p-4 shadow rounded-md"
+        style={{height:"33%"}}
       >
         <img
           src={`${baseurl}/uploads/${owner.profilePhoto}`}
           alt="Mess"
-          className="w-full md:w-1/3 object-cover rounded-md"
+          className="w-full md:[width:26%] object-cover rounded-md"
         />
         <div className="md:ml-6 mt-4 md:mt-0">
           <h3 className="font-medium text-lg">{owner.messName}, In Simhat</h3>
@@ -69,6 +71,9 @@ function MessBars({ isChecked /* ,checkFeatures}*/ }) {
             <span>Single Bed</span>
             <span className="mx-2">•</span>
             <span>4+ More</span>
+          </div>
+          <div>
+            <span>price : 2.5k/Month</span>
           </div>
           <div className="flex gap-4 mt-4">
             <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
