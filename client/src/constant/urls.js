ac2ed68846a8fb58cf0fc81aa20a8f2a667c490c
sayan patra <sayanpatra017@gmail.com>
2024-12-14 01:55:06 +0530

    dd

--- client/src/constant/urls.js
+++ client/src/constant/urls.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://easypg-server.vercel.app/'
 
 export const loginUrl = `${baseUrl}/auth/login`
 export const signupUrl = `${baseUrl}/auth/signup`
