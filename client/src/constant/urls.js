53deb20ba1530dfa8f9baf6e1ed31eb69093cec9
sayan patra <sayanpatra017@gmail.com>
2024-12-14 03:46:26 +0530

    reverting

--- client/src/constant/urls.js
+++ client/src/constant/urls.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://localhost:8080'
+const baseUrl = 'http://localhost:8080'
 
 export const loginUrl = `${baseUrl}/auth/login`
 export const signupUrl = `${baseUrl}/auth/signup`
