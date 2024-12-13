3f5acfa7cb6f282ea29771f2f3bf3f6f73a1a207
sayan patra <sayanpatra017@gmail.com>
2024-12-14 05:03:56 +0530

    deploy rgfhjrender2

--- client/src/constant/urls.js
+++ client/src/constant/urls.js
@@ -1,4 +1,4 @@
-const baseUrl = ' http://localhost:10000/'
+const baseUrl = ' https://easypg-react.onrender.com/'
 
 export const loginUrl = `${baseUrl}/auth/login`
 export const signupUrl = `${baseUrl}/auth/signup`
