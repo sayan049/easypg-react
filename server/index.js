72a2ce6661d3b668ae047809a503f91cf8521938
sayan patra <sayanpatra017@gmail.com>
2024-12-19 00:55:27 +0530

    ksfmggfd

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8080;
 const corsOptions = {
   origin: ORIGIN,
   credentials: true,
-  methods: ['GET', 'POST'],
+  
   
 };
 app.set('trust proxy', 1); // Trust the first proxy
