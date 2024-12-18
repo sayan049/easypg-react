7b2553537c0443fcb566dea9edf3cce3663a190f
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-18 22:24:05 +0530

    impl map

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Mao,GoogleApiWrapper} from 'google-maps-react';
 // import axios from 'axios';
 // import { findMessUrl } from '../constant/urls';
 // import L from 'leaflet';
@@ -27,53 +28,30 @@ function MapComponent({isChecked}) {
     //                     console.log("Parsed latitude: ", latitude);
     //                     console.log("Parsed longitude: ", longitude);
 
-    //                     if (!mapRef.current) {
-    //                         // Initialize the map only if it hasn't been initialized already
-    //                         mapRef.current = L.map('map').setView([latitude, longitude], 13);
-    //                         console.log("Map initialized");
-
-    //                         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //                             maxZoom: 19,
-    //                         }).addTo(mapRef.current);
-    //                         console.log("Tile layer added to map");
-
-    //                         // Create a custom icon
-    //                         const customIcon = L.icon({
-    //                             iconUrl: markerIcon,
-    //                             shadowUrl: markerShadow,
-    //                             iconSize: [25, 41], // size of the icon
-    //                             iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
-    //                             popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
-    //                             tooltipAnchor: [16, -28], // point from which the tooltip should open relative to the iconAnchor
-    //                             shadowSize: [41, 41] // size of the shadow
-    //                         });
-
-    //                         console.log("Custom icon created");
-
-    //                         // Add a marker with the custom icon
-    //                         L.marker([latitude, longitude], { icon: customIcon }).addTo(mapRef.current);
-    //                         console.log("Marker added to map");
-    //                     } else {
-    //                         // Update the map view if it already exists
-    //                         mapRef.current.setView([latitude, longitude], 13);
-    //                     }
-    //                 } else {
-    //                     console.error("Location data is missing");
-    //                 }
-    //             } else {
-    //                 console.error("No locations found in the response");
-    //             }
-    //         } catch (error) {
-    //             console.error("Error fetching data:", error);
-    //         }
+    //                     
     //     };
 
     //     fetchData();
     // }, []); // Empty dependency array ensures it runs only once
 
     return (
-        <div id="map" style={{ height: '84vh', width: '35vw' , display: isChecked ? "block" : "none" }} >xyz</div>
+        <div id="map" style={{ height: '84vh', width: '35vw' , display: isChecked ? "block" : "none" }} >
+            <Map
+            google={this.props.google}
+            zoom={10}
+            style={{width:'100%', height:'100%'}}
+            initialCentre={{
+                lat: 22.958622435430872,
+                lng:88.54578601291212
+            }}
+            />
+            
+            </div>
     );
 }
 
-export default MapComponent;
+//export default MapComponent;
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAlJ2p7ePie8E9JH4TeouoayKAvathIGr0', 
+  })(MapComponent);
