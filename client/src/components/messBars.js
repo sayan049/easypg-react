62069693f172e9b1e26314fe5ce32b211c54480f
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-22 21:57:45 +0530

    redirect

--- client/src/components/messBars.js
+++ client/src/components/messBars.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { baseurl, findMessUrl } from '../constant/urls';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 function MessBars({ isChecked, checkFeatures, coords }) {
   const [messData, setMessData] = useState([]);
   const [error, setError] = useState(null);
+  const navigate=useNavigate();
 
   const clickCords = (location) => {
     // Check if location is a valid string
@@ -17,6 +19,10 @@ function MessBars({ isChecked, checkFeatures, coords }) {
       console.log('Invalid location:', location); // Handle invalid location
     }
   };
+
+  const clickNavi=()=>{
+    navigate('/viewDetails');
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -81,9 +87,8 @@ function MessBars({ isChecked, checkFeatures, coords }) {
               <span>price : 2.5k/Month</span>
             </div>
             <div className="flex gap-4 mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={clickNavi}>
                 View Details
-                <Link to='/viewDetails'></Link>
               </button>
               <button className="bg-green-500 text-white px-4 py-2 rounded-md">
                 Book Now
