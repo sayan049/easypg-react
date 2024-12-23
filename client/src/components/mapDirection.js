a49568f30886c5acf346ce456eeeebfab39a10ca
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-24 02:57:08 +0530

    map implement

--- client/src/components/mapDirection.js
+++ client/src/components/mapDirection.js
@@ -24,7 +24,7 @@ function MapDirection({ isChecked, coordinates }) {
     // Ensure coordinates are numbers
     const { lat, lng } = coordinates;
     const centerCoordinates = fromLonLat([lng, lat]);
-    console.log("fuck"+coordinates);
+    console.log("fuck"+coordinates+" "+lng+" "+lat+" "+centerCoordinates);
     // Create a marker feature
     const marker = new Feature({
       geometry: new Point(centerCoordinates),
