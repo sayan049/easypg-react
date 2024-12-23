f353a2d8c1aa121f13d9f264db2046789c3c3fa1
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-24 01:54:10 +0530

    map implement

--- client/src/pages/viewDetails.jsx
+++ client/src/pages/viewDetails.jsx
@@ -5,7 +5,11 @@ import MapDirection from "../components/mapDirection";
 const ViewDetails = () => {
   const location=useLocation();
   const {owner}=location.state || {};
-  const coordinates=owner.location.split(',');
+  const locationArray = owner.location.split(',');
+const coordinates = {
+  lat: parseFloat(locationArray[1]), // Latitude
+  lng: parseFloat(locationArray[0]), // Longitude
+};
   return (
     <div className="max-w-5xl mx-auto bg-white shadow-md rounded-lg overflow-hidden my-8">
       
