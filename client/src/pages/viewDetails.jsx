c90e17cb06ff23282452380784b042784c209a1a
myself-rishi-saha <rishisaha302016@gmail.com>
2024-12-24 20:03:20 +0530

    button fix

--- client/src/pages/viewDetails.jsx
+++ client/src/pages/viewDetails.jsx
@@ -127,7 +127,11 @@ const ViewDetails = () => {
 
         {/* Map Section */}
         <div className="mb-6">
-          <h2 className="text-lg font-semibold mb-4">What's nearby?</h2> <button>click me</button>
+          <h2 className="text-lg font-semibold mb-4">What's nearby?</h2> 
+          <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300">
+  Click Me
+</button>
+
           <MapDirection coordinates={coordinates} />
         </div>
       </div>
