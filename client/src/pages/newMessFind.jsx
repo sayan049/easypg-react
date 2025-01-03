49c1b36732d47771fcb01bb6c67dbc26e50a08b6
myself-rishi-saha <rishisaha302016@gmail.com>
2025-01-03 22:27:58 +0530

    responsive site

--- client/src/pages/newMessFind.jsx
+++ client/src/pages/newMessFind.jsx
@@ -41,6 +41,16 @@ const FilterModal = ({ isOpen, onClose, price, setPrice, amenities, featureChang
               {facility.label}
             </label>
           ))}
+
+
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded shadow"
+            onClick={onClose}
+          >
+            Apply Filters
+          </button>
+
+          
         </div>
         {/* Close Button */}
         <div className="mt-6 flex justify-end">
