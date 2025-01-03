9eba8ced1226b3684cedff36e868cda038a9aaa0
sayan049 <sayanpatra019@gmail.com>
2025-01-03 23:06:43 +0530

    reset password 3

--- client/src/pages/signupUser.jsx
+++ client/src/pages/signupUser.jsx
@@ -45,10 +45,10 @@ function SignUpForm() {
         localStorage.setItem("loginMessage", message);
         navigate("/LoginUser", { state: { message } });
       } else {
-        console.error("Signup failed:", response.data);
+        return error("Signup failed:", response.data);
       }
     } catch (error) {
-      console.error("Error sending JSON data:", error);
+     return console.error("Error sending JSON data:", error);
     } finally {
       // Re-enable the button after 5 seconds
       setTimeout(() => {
