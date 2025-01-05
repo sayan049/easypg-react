d7d39c866a22a19f769e62e6a2f916b0b6e5a02a
myself-rishi-saha <rishisaha302016@gmail.com>
2025-01-05 20:49:06 +0530

    dropdown

--- client/src/components/dropdown.js
+++ client/src/components/dropdown.js
@@ -51,16 +51,15 @@ function Dropdown() {
 
   return (
     <nav className="relative">
-      {/* Dropdown Button */}
+      {/* Dropdown Button with only the icon */}
       <button
         onClick={toggleDisplay}
-        className="flex items-center justify-between px-4 py-2 rounded-md text-sm font-semibold text-gray-700 bg-white border border-gray-300 w-full md:w-auto"
+        className="flex items-center justify-center p-2 rounded-md text-gray-700 bg-white border border-gray-300 w-full md:w-auto"
       >
-        <span>Sort By</span>
         <img
           src="./assets/down-arrow.png" // Use your dropdown icon here
           alt="Dropdown"
-          className="w-4 h-4 ml-2"
+          className="w-4 h-4"
         />
       </button>
 
