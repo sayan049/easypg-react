15b8e34e21a2daa3f1c18c4b5b4b486ee98b32a2
sayan049 <sayanpatra019@gmail.com>
2025-01-07 21:05:59 +0530

    gggg

--- server/middleware/is-auth.js
+++ server/middleware/is-auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateJWT = (req, res, next) => {
-  const token = req.headers.Authorization?.split(" ")[1]; // Extract token from Authorization header
+  const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
 
   if (!token) {
     return res.status(401).json({ message: "Access token is missing." });
