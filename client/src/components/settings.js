b3485b96718026455bfd39866536ddb36a364296
deepxyz2 <deepsaha302016@gmail.com>
2025-03-27 21:53:16 +0530

    personal info

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -190,7 +190,7 @@ function Settings() {
           />
           {image ? (
             <img
-              src={URL.createObjectURL(personalInfo?.image||image)}
+              src={user?.image||URL.createObjectURL(image)}
               alt="Profile"
               className="w-36 h-36 rounded-full object-cover mx-auto mt-4 border-4 border-[#2ca4b5]"
             />
