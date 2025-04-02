2b2e49fdcece8c25c2a78568a5e36ce893822623
deepxyz2 <deepsaha302016@gmail.com>
2025-04-03 02:27:03 +0530

    reset password

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -247,7 +247,7 @@ function Settings() {
       <div className="relative mb-4"></div>
       {/* Profile Picture Section (First Row) */}
       <div className="flex justify-center mb-6">
-        <div className="text-center hidden flex slign-centre flex-col">
+        <div className="text-center flex slign-centre flex-col">
           <input
             type="file"
             accept="image/*"
