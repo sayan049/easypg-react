c4f2d407b11f6d282676db6549fd64d837608762
deepxyz2 <deepsaha302016@gmail.com>
2025-04-02 23:50:39 +0530

    finishing

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -83,7 +83,7 @@ function Settings() {
       if (personalInfo[key]) {
         formData.append(key, personalInfo[key]);
       }
-      setEditingField(editingField === field ? null : field);
+      setEditingField(editingField === key ? null : key);
       setIsEditing(!isEditing);
     });
 
