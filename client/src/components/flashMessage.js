d6eed18d2162c86294694f81e3716dac02417d2c
deepxyz2 <deepsaha302016@gmail.com>
2025-04-20 20:19:09 +0530

    flash message

--- client/src/components/flashMessage.js
+++ client/src/components/flashMessage.js
@@ -38,15 +38,49 @@ function FlashMessage({ message, type = 'info' }) {
 
 
   return (
-    <div className={`${baseStyles} ${typeStyles[type]} max-w-md mx-auto my-4 absolute`}>
-      <span className="material-icons-outlined">
-        {type === 'success' && 'check_circle'}
-        {type === 'error' && 'error'}
-        {type === 'warning' && 'warning'}
-        {type === 'info' && 'info'}
-      </span>
-      <span>{message}</span>
+    <div role="alert" className="rounded-md border border-gray-300 bg-white p-4 shadow-sm">
+    <div className="flex items-start gap-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6 text-green-600"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+  
+      <div className="flex-1">
+        <strong className="font-medium text-gray-900"> Successfully signed up</strong>
+  
+        <p className="mt-0.5 text-sm text-gray-700">Please verify your email</p>
+      </div>
+  
+      <button
+        className="-m-3 rounded-full p-1.5 text-gray-500 transition-colors hover:bg-gray-50 hover:text-gray-700"
+        type="button"
+        aria-label="Dismiss alert"
+      >
+        <span className="sr-only">Dismiss popup</span>
+  
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
     </div>
+  </div>
   );
 }
 
