759a43f8270242b2c84678c0017236956d604391
deepxyz2 <deepsaha302016@gmail.com>
2025-04-21 12:40:57 +0530

    hide flash

--- client/src/components/flashMessage.js
+++ client/src/components/flashMessage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-function FlashMessage({ message='xxx', type = 'error' }) {
+function FlashMessage({ message='xxx', type = error }) {
   const [show, setShow] = React.useState(true);
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -18,14 +18,6 @@ function FlashMessage({ message='xxx', type = 'error' }) {
   console.log('FlashMessage', message, type);
 
 
-
-  const baseStyles = 'p-4 rounded-2xl shadow-md flex items-center gap-3 text-sm sm:text-base';
-  const typeStyles = {
-    success: 'bg-green-100 text-green-800 border border-green-300',
-    error: 'bg-red-100 text-red-800 border border-red-300',
-    warning: 'bg-yellow-100 text-yellow-800 border border-yellow-300',
-    info: 'bg-blue-100 text-blue-800 border border-blue-300',
-  };
   // if (message == null){
   //   return null;
   // }
