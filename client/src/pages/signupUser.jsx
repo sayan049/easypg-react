2a599d33834f21b0c23e2fe6d6f32c088b3639af
deepxyz2 <deepsaha302016@gmail.com>
2025-04-23 21:59:02 +0530

    toast

--- client/src/pages/signupUser.jsx
+++ client/src/pages/signupUser.jsx
@@ -80,7 +80,7 @@ function SignUpForm() {
   
     try {
       const res = await axios.get(url);
-      console.log(res.data);
+      console.log(res.data,url,accessKey);
       return true;
     } catch (err) {
       console.error("Error validating email:", err);
