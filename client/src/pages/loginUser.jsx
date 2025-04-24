bd1f310b71a0570c0d6c487c1105af9ca879e65f
deepxyz2 <deepsaha302016@gmail.com>
2025-04-25 00:51:25 +0530

    toast login

--- client/src/pages/loginUser.jsx
+++ client/src/pages/loginUser.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation,useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { loginUrl, forgotPasswordUserUrl, resetPasswordUserUrl,tokenVerifyUserUrl, baseurl } from "../constant/urls";
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // import Skeleton from "react-loading-skeleton";
 // import "react-loading-skeleton/dist/skeleton.css";
 
@@ -105,6 +107,7 @@ const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
       }
     } catch (error) {
       console.log("Error sending JSON data:", error);
+      toast.error(error.response?.data?.message || res.data.errors.join(", ")|| "Login failed. Please try again.");
     } finally {
       setTimeout(() => {
         setIsButtonDisabled(false);
@@ -177,6 +180,7 @@ const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
   return (
     <div className="flex flex-col lg:flex-row h-screen bg-custom-gradient">
+      <ToastContainer/>
       
       {/* Left Section */}
       <div className="flex-1 lg:w-8/12 flex items-center justify-center p-6">
