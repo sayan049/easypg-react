8017a013f66c14a70a43a4e412acc5b5f31e0e97
deepxyz2 <deepsaha302016@gmail.com>
2025-04-26 03:21:48 +0530

    submitting

--- client/src/components/messBars.js
+++ client/src/components/messBars.js
@@ -25,9 +25,9 @@ function MessBars({
     navigate("/booking", { state: { owner } });
   };
 
-  const getDistanceFromLatLonInKm=(lat1, lon1, lat2, lon2)=> {
+  const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
     if (!lat1 || !lon1 || !lat2 || !lon2) {
-      return null;;
+      return null;
     }
     const R = 6371; // Radius of Earth in km
     const dLat = (lat2 - lat1) * Math.PI / 180;
@@ -38,8 +38,8 @@ function MessBars({
       Math.cos(lat2 * Math.PI / 180) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return( R * c.toFixed(2)); // Distance in km
-  }
+    return parseFloat((R * c).toFixed(1)); // Return as number
+  };
   
 
   
