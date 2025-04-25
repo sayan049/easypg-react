748c0798fcde9f190263f2a360ee272ad04bdc9c
deepxyz2 <deepsaha302016@gmail.com>
2025-04-26 01:59:17 +0530

    submitting

--- client/src/pages/signupOwner.jsx
+++ client/src/pages/signupOwner.jsx
@@ -30,8 +30,8 @@ function SignupOwner() {
 
   useEffect(() => {
     document.title = "Sign up for owner";
-    console.log(isFormComplete());
-  }, [isFormComplete]);
+   // console.log(isFormComplete());
+  }, []);
 
   const mapMake = () => {
     if (navigator.geolocation) {
