b51596d4cd2794ed45355489ce7df0f40eb53423
sayan049 <sayanpatra019@gmail.com>
2025-04-27 00:39:10 +0530

    updated

--- client/src/pages/newDashBoard.jsx
+++ client/src/pages/newDashBoard.jsx
@@ -130,6 +130,14 @@ import {
 import axios from "axios";
 import { baseurl, fetchDetailsUrl } from "../constant/urls";
 import { toast } from "react-toastify";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Sidebar from "../components/sidebar";
+import BookingTable from "../components/BookingTable";
+import Settings from "../components/settings";
+import DashboardContent from "../components/dashboardContent";
+
+
 
 function NewDashboard() {
   const [activeTab, setActiveTab] = useState("dashboard");
