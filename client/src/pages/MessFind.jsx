33506e52251dfd90d922e84688f1e0e180ed7e7d
deepxyz2 <deepsaha302016@gmail.com>
2025-04-28 01:24:53 +0530

    messfind responsive

--- client/src/pages/MessFind.jsx
+++ client/src/pages/MessFind.jsx
@@ -71,14 +71,11 @@ const MessFind = () => {
   const userLocation = location.state?.userLocation || null;
   const item = location.state?.item || null; // Retrieve items from Homepage
   // Retrieve lat/lon from Homepage
-  const { 
-    performSearch, 
-    handleInputChange, 
-    setSearchItem 
-  } = location.state || {};
+  const { performSearch, handleInputChange, setSearchItem } =
+    location.state || {};
 
   // Local state for search input
- 
+
   console.log("🔍 Full Navigation State in MessFind:", location.state);
   console.log("📍 Extracted User Location:", userLocation);
 
@@ -203,7 +200,10 @@ const MessFind = () => {
 
           {/* implement a search bar here  */}
           <div className="flex items-center gap-4 bg-white p-3 rounded-lg border border-gray-300 shadow-sm hover:border-blue-500 transition-colors w-full">
-            <FaSearch className="w-5 h-5 text-gray-400" onClick={() => performSearch && performSearch()} />
+            <FaSearch
+              className="w-5 h-5 text-gray-400"
+              onClick={() => performSearch && performSearch()}
+            />
             <input
               type="text"
               placeholder="Search mess by location"
@@ -214,7 +214,9 @@ const MessFind = () => {
                 if (handleInputChange) handleInputChange(e);
                 if (setSearchItem) setSearchItem(e.target.value);
               }}
-              onKeyPress={(e) => e.key === "Enter" && performSearch && performSearch()}
+              onKeyDown={(e) =>
+                e.key === "Enter" && performSearch && performSearch()
+              }
             />
           </div>
 
