ebb48917a78f7222424fdb5f701bfcce1fbe0247
deepxyz2 <deepsaha302016@gmail.com>
2025-04-27 15:33:41 +0530

    road dist

--- client/src/components/messBars.js
+++ client/src/components/messBars.js
@@ -167,7 +167,7 @@ function MessBars({
         {messData.map((owner) => (
           <div
             key={owner._id}
-            className={`relative flip-card h-[15rem] ${
+            className={`relative flip-card h-[31rem] md:h-[15rem] ${
               selected === owner._id && isChecked
                 ? "ring-2 ring-blue-500"
                 : "ring-1 ring-gray-200"
