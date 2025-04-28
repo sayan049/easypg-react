d8c7f0ca3dd55cdcbed56c8a319c98b73aa62319
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-04-28 20:35:30 +0530

    facilities

--- client/src/pages/MessFind.jsx
+++ client/src/pages/MessFind.jsx
@@ -362,10 +362,7 @@ const MessFind = () => {
         <div className="flex flex-row md:flex-row justify-between items-center bg-white p-4 shadow rounded-md relative">
           {/* Search Bar */}
           <div className="flex items-center gap-4 bg-white p-3 rounded-lg border border-gray-300 shadow-sm hover:border-blue-500 transition-colors w-full relative">
-            <FaSearch
-              className="w-5 h-5 text-gray-400 cursor-pointer"
-              onClick={performSearch}
-            />
+            
             <input
               type="text"
               placeholder="Search mess by location"
@@ -373,6 +370,10 @@ const MessFind = () => {
               value={searchQuery}
               onChange={handleInputChange}
               onKeyDown={(e) => e.key === "Enter" && performSearch()}
+            />
+            <FaSearch
+              className="w-5 h-5 text-gray-400 cursor-pointer"
+              onClick={performSearch}
             />
 
             {/* Suggestions Dropdown */}
