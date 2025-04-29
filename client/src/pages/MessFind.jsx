e9f16bc2a867f02c3a63e28903a26bf185d54477
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-04-29 19:31:26 +0530

    facilities

--- client/src/pages/MessFind.jsx
+++ client/src/pages/MessFind.jsx
@@ -193,7 +193,7 @@ const MessFind = () => {
       lat: suggestion.lat,
       lng: suggestion.lon,
     });
-    //setSearchQuery(suggestion.display_name);
+  //  setSearchQuery(suggestion.display_name);
  
   setSuggestions([]);
 
@@ -380,6 +380,7 @@ const MessFind = () => {
               type="text"
               placeholder="Search mess by location"
               className="w-full p-1 text-base outline-none placeholder-gray-400"
+            //  value={searchQuery}
               onChange={handleInputChange}
               onKeyDown={(e) => e.key === "Enter" && performSearch()}
             />
