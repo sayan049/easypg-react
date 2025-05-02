761316f7d7ee98dae9c40268ed3148fe4e2944b2
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-02 20:39:04 +0530

    search func

--- client/src/components/messBars.js
+++ client/src/components/messBars.js
@@ -285,11 +285,11 @@ function MessBars({
                       className="w-full h-full object-cover"
                     />
                     {owner.roomInfo?.some((room) => room.roomAvailable) ? (
-                      <div className="absolute bottom-2 right-2 bg-green-500 text-white text-xs px-2 py-1 rounded">
+                      <div className="absolute bottom-2 right-2 border-green-400 bg-white text-green-400 text-xs px-2 py-1 rounded">
                         Available
                       </div>)
                       :(
-                        <div className="absolute bottom-2 right-2 bg-red-500 text-white text-xs px-2 py-1 rounded">
+                        <div className="absolute bottom-2 right-2 border-red-400 bg-white text-red-400 text-xs px-2 py-1 rounded">
                           Not Available
                         </div>
                     )}
