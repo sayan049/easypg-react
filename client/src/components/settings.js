a4b6c7b76cc7c6dd47cb6a62ed370066f6d27900
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-03 17:42:04 +0530

    settings

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -625,7 +625,7 @@
 // }
 
 // export default Settings;
-import  { useState, useEffect } from "react";
+import  { React, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMapMarkerAlt,
