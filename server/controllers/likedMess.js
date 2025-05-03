f8d5c50e6b555d296f3e2db83570449dbfce1c39
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-03 23:59:11 +0530

    sett

--- server/controllers/likedMess.js
+++ server/controllers/likedMess.js
@@ -1,7 +1,8 @@
 const User = require("../modules/user");
+const pgProvider = require("../modules/pgProvider");
 
 const likedMess = async (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
   const { messId, liked } = req.body;
   console.log(userId, messId, liked, "likedMesses");
 
@@ -21,3 +22,26 @@ const likedMess = async (req, res) => {
 };
 
 module.exports = likedMess;
+
+const cartMess = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const user = await User.findById(userId).populate("likedMesses");
+    if (!user) {
+      req.status(400, { message: "couldn't find user" });
+    }
+    const likedMesses = user.likedMesses;
+    if (!likedMesses || likedMesses.length === 0) {
+      res.status(401, { message: "no liked messes" });
+    }
+    //res.status(200).json(likedMesses);
+    const messOwners = await pgProvider.find({ _id: { $in: likedMesses } });
+
+    return res.status(200).json(messOwners);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" }); // Handle server errors
+  }
+};
+
+module.exports = cartMess;
