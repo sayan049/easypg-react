591d85139215b011174bb2e53d086c449eb888e7
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-09 19:59:11 +0530

    signup owner

--- client/src/pages/signupOwner.jsx
+++ client/src/pages/signupOwner.jsx
@@ -1328,7 +1328,7 @@ function SignupOwner() {
         pauseOnHover
       />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-custom-gradient">
         {/* Header */}
         <header className="flex items-center justify-between mb-8">
           <div className="flex items-center gap-4">
