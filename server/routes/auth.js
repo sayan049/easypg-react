b91b228ab8a8d8a03e84363c6d9f9489ad01564b
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-15 23:53:10 +0530

    auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../modules/user');
-const PgOwner = require('../modules/pgProvider');
+const User = require("../modules/user");
+const PgOwner = require("../modules/pgProvider");
 const authHandlers = require("../controllers/authHandlers");
 const { uploadTemp, uploadToCloudinary } = require("../middleware/upload");
 // const { refreshTokenHandler } = require("../controllers/refreshTokenHandler");
-const refreshToken = require("../controllers/refreshTokenHandler")
-const updateDetailshandler =require("../controllers/updateDetails")
-const forgotPasswordUser = require("../controllers/forgotPasswordUser")
-const resetPasswordUser = require("../controllers/resetPasswordUser")
-const authenticateJWT = require("../middleware/is-auth")
-const forgotPasswordOwner = require("../controllers/forgotPasswordOwner")
-const resetPasswordOwner = require("../controllers/resetPasswordOwner")
-const resetPasswordDashboard=require("../controllers/resetPasswordDashboard")
-const  updatePassword  =require("../controllers/updatePasswordDashboardOwner");
+const refreshToken = require("../controllers/refreshTokenHandler");
+const updateDetailshandler = require("../controllers/updateDetails");
+const forgotPasswordUser = require("../controllers/forgotPasswordUser");
+const resetPasswordUser = require("../controllers/resetPasswordUser");
+const authenticateJWT = require("../middleware/is-auth");
+const forgotPasswordOwner = require("../controllers/forgotPasswordOwner");
+const resetPasswordOwner = require("../controllers/resetPasswordOwner");
+const resetPasswordDashboard = require("../controllers/resetPasswordDashboard");
+const updatePassword = require("../controllers/updatePasswordDashboardOwner");
 const likedMess = require("../controllers/likedMess");
-const frontendUrl = process.env.CLIENT_URL || 'http://localhost:3000';
+const frontendUrl = process.env.CLIENT_URL || "http://localhost:3000";
 const getLikedMess = require("../controllers/getLikedMess");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const {
   createBookingRequest,
   handleBookingApproval,
-  
+
   getOwnerBookings,
   getUserBookings,
   downloadInvoice,
@@ -33,9 +33,8 @@ const {
   cancelBooking,
   submitFeedback,
   getOwnerDashboardStats,
-  getChartStats
-} = require('../controllers/bookingController');
-
+  getChartStats,
+} = require("../controllers/bookingController");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
@@ -44,9 +43,8 @@ const apiKey = process.env.GOOGLE_PLACES;
 
 router.get("/api/autocomplete", async (req, res) => {
   const { input } = req.query;
-console.log("Input:", input);
-const url = `https://api.locationiq.com/v1/autocomplete?key=${apiKey}&q=${input}&countrycodes=IN`;
-
+  console.log("Input:", input);
+  const url = `https://api.locationiq.com/v1/autocomplete?key=${apiKey}&q=${input}&countrycodes=IN`;
 
   try {
     const response = await fetch(url);
@@ -61,16 +59,26 @@ const url = `https://api.locationiq.com/v1/autocomplete?key=${apiKey}&q=${input}
     console.error("Error fetching data from LocationIQ API:", error);
     res.status(500).send("Error fetching data");
   }
-  
 });
 router.post("/signup", authHandlers.signupHandler);
 router.post("/resend-verification", authHandlers.resendVerificationEmail);
-router.get('/check-email-verification', authHandlers.checkEmailVerification);
+router.get("/check-email-verification", authHandlers.checkEmailVerification);
 
 router.post("/login", authHandlers.loginHandler);
-router.post("/signupOwner",uploadTemp, uploadToCloudinary, authHandlers.signupHandlerOwner);
-router.get("/check-email-verification-owner", authHandlers.checkEmailVerificationOwner);
-router.post("/resend-verification-owner", authHandlers.resendVerificationEmailOwner);
+router.post(
+  "/signupOwner",
+  uploadTemp,
+  uploadToCloudinary,
+  authHandlers.signupHandlerOwner
+);
+router.get(
+  "/check-email-verification-owner",
+  authHandlers.checkEmailVerificationOwner
+);
+router.post(
+  "/resend-verification-owner",
+  authHandlers.resendVerificationEmailOwner
+);
 
 router.post("/loginOwner", authHandlers.loginHandlerOwner);
 router.post("/likedMess", authenticateJWT, likedMess.likedMess);
@@ -78,56 +86,69 @@ router.get("/getLikedMess", authenticateJWT, getLikedMess);
 router.get("/findMess", authHandlers.findMess);
 router.get("/getRecc", authHandlers.getTopRatedMesses);
 router.get("/viewDetails/:messId", authHandlers.viewDetails);
-router.get("/getCart",authenticateJWT,likedMess.cartMess);
-
+router.get("/getCart", authenticateJWT, likedMess.cartMess);
 
 // router.get("/protected", ensureAuthenticated, (req, res) => {
 //   res.json({ message: "This is a protected route", user: req.session.user });
 // });
 
-router.post("/refresh-token",refreshToken.refreshTokenHandler);
-router.get("/getRefreshToken",refreshToken.getRefreshToken);
+router.post("/refresh-token", refreshToken.refreshTokenHandler);
+router.get("/getRefreshToken", refreshToken.getRefreshToken);
 
-router.get("/check-session", (req, res) => {
-  const accessToken = req.headers['authorization']?.split(' ')[1]; // Get token from Authorization header
- 
-
-  if (!accessToken) {
-    return res.status(401).json({ isAuthenticated: false, message: "Access token is required." });
-  }
+router.get("/check-session", authenticateJWT, (req, res) => {
+  // const accessToken = req.headers['authorization']?.split(' ')[1]; // Get token from Authorization header
+  const accessToken = req.cookies?.accessToken;
 
   // Verify the access token
-  jwt.verify(accessToken, JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ isAuthenticated: false, message: "Invalid or expired access token." });
-    }
+  // jwt.verify(accessToken, JWT_SECRET, (err, decoded) => {
+  //   if (err) {
+  //     return res.status(401).json({ isAuthenticated: false, message: "Invalid or expired access token." });
+  //   }
 
-    // console.log(decoded.id, decoded.email, decoded.type, decoded.name);
+  //   // console.log(decoded.id, decoded.email, decoded.type, decoded.name);
 
-    // Prepare response
-    const userResponse = {
-      isAuthenticated: true,
-      user: { id: decoded.id, email: decoded.email, type: decoded.type, name: decoded.name },
-      loginMethod: decoded.loginMethod,
-    };
+  //   // Prepare response
+  //   const userResponse = {
+  //     isAuthenticated: true,
+  //     user: { id: decoded.id, email: decoded.email, type: decoded.type, name: decoded.name },
+  //     loginMethod: decoded.loginMethod,
+  //   };
 
-    // If user is an owner, attach the image
-    if (decoded.loginMethod === 'google') {
-      userResponse.user.image = decoded.image; // Assuming the image is stored in the decoded token
-    }
+  //   // If user is an owner, attach the image
+  //   if (decoded.loginMethod === 'google') {
+  //     userResponse.user.image = decoded.image; // Assuming the image is stored in the decoded token
+  //   }
 
-    // Send the response with the user data
-    return res.status(200).json(userResponse);
-  });
+  //   // Send the response with the user data
+  //   return res.status(200).json(userResponse);
+  // });
+  const userResponse = {
+    isAuthenticated: true,
+    user: {
+      id: decoded.id,
+      email: decoded.email,
+      type: decoded.type,
+      name: decoded.name,
+    },
+    loginMethod: decoded.loginMethod,
+  };
+
+  // If user is an owner, attach the image
+  if (decoded.loginMethod === "google") {
+    userResponse.user.image = decoded.image; // Assuming the image is stored in the decoded token
+  }
+
+  // Send the response with the user data
+  return res.status(200).json(userResponse);
 });
 
-
-
-
-
-
-router.post('/updateDetails',uploadTemp, uploadToCloudinary, updateDetailshandler.updateDetails);
-router.get('/get-details', updateDetailshandler.getDetails);
+router.post(
+  "/updateDetails",
+  uploadTemp,
+  uploadToCloudinary,
+  updateDetailshandler.updateDetails
+);
+router.get("/get-details", updateDetailshandler.getDetails);
 
 router.post("/logout", authenticateJWT, async (req, res) => {
   try {
@@ -142,7 +163,9 @@ router.post("/logout", authenticateJWT, async (req, res) => {
     const refreshToken = req.body.refreshToken; // Assumes the refresh token is sent in the body
 
     if (!refreshToken) {
-      return res.status(400).json({ message: "Refresh token is required to log out." });
+      return res
+        .status(400)
+        .json({ message: "Refresh token is required to log out." });
     }
 
     let user;
@@ -151,9 +174,11 @@ router.post("/logout", authenticateJWT, async (req, res) => {
       if (!user) {
         return res.status(404).json({ message: "User not found." });
       }
-      const device = req.headers['x-device-info'];
+      const device = req.headers["x-device-info"];
       // Remove the refresh token from the stored tokens array based on the token provided
-      user.refreshTokens = user.refreshTokens.filter(rt => !(rt.token === refreshToken && rt.device === device));
+      user.refreshTokens = user.refreshTokens.filter(
+        (rt) => !(rt.token === refreshToken && rt.device === device)
+      );
 
       await user.save();
     } else if (type === "owner") {
@@ -163,7 +188,9 @@ router.post("/logout", authenticateJWT, async (req, res) => {
       }
 
       // Remove the refresh token from the stored tokens array based on the token provided
-      user.refreshTokens = user.refreshTokens.filter(rt => rt.token !== refreshToken);
+      user.refreshTokens = user.refreshTokens.filter(
+        (rt) => rt.token !== refreshToken
+      );
       await user.save();
     } else {
       return res.status(400).json({ message: "Invalid user type." });
@@ -172,106 +199,120 @@ router.post("/logout", authenticateJWT, async (req, res) => {
     return res.status(200).json({ message: "Logged out successfully." });
   } catch (error) {
     console.error("Logout error:", error);
-    return res.status(500).json({ message: "An error occurred during logout." });
+    return res
+      .status(500)
+      .json({ message: "An error occurred during logout." });
   }
 });
 
 router.post("/user/forgot-password", forgotPasswordUser);
 
-
-
-router.get('/LoginUser/user/reset-password/:resetToken', (req, res) => {
+router.get("/LoginUser/user/reset-password/:resetToken", (req, res) => {
   const resetToken = req.params.resetToken;
 
   // Verify the token
   jwt.verify(resetToken, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(400).json({ message: 'Invalid or expired token' });
+      return res.status(400).json({ message: "Invalid or expired token" });
     }
 
     // Send the URL to the frontend instead of redirecting
     return res.json({
-      message: 'Token is valid',
+      message: "Token is valid",
       // resetUrl: `https://easypg-react-client.onrender.com/LoginUser?resetToken=${resetToken}`,
       resetUrl: `${frontendUrl}/login/user?resetToken=${resetToken}`,
     });
   });
 });
 
-
-
-router.post("/user/reset-password",resetPasswordUser);
+router.post("/user/reset-password", resetPasswordUser);
 
 //----------------------------------------------------------------------------------->
 router.post("/owner/forgot-password", forgotPasswordOwner);
 
-router.get('/LoginOwner/owner/reset-password/:resetToken', (req, res) => {
+router.get("/LoginOwner/owner/reset-password/:resetToken", (req, res) => {
   const resetToken = req.params.resetToken;
 
   // Verify the token
   jwt.verify(resetToken, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(400).json({ message: 'Invalid or expired token' });
+      return res.status(400).json({ message: "Invalid or expired token" });
     }
 
     // Send the URL to the frontend instead of redirecting
     return res.json({
-      message: 'Token is valid',
+      message: "Token is valid",
       resetUrl: `${frontendUrl}/login/owner?resetToken=${resetToken}`,
     });
   });
 });
 
-
-
-router.post("/owner/reset-password",resetPasswordOwner);
-router.post("/resetPasswordDashboard",resetPasswordDashboard);
-router.post("/updatePasswordDashboardOwner",  async (req, res) => {
-  const { userId,currentPassword, newPassword, confirmPassword } = req.body;
-
+router.post("/owner/reset-password", resetPasswordOwner);
+router.post("/resetPasswordDashboard", resetPasswordDashboard);
+router.post("/updatePasswordDashboardOwner", async (req, res) => {
+  const { userId, currentPassword, newPassword, confirmPassword } = req.body;
 
   try {
-    const result = await updatePassword(userId, currentPassword, newPassword, confirmPassword);
+    const result = await updatePassword(
+      userId,
+      currentPassword,
+      newPassword,
+      confirmPassword
+    );
     res.status(200).json(result);
   } catch (error) {
     console.error("Error updating password:", error);
-    res.status(500).json({ error: "An error occurred while updating password" });
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating password" });
   }
 });
-
 
 //booking part----------------------------------------------------------------------------------->
 
 // User initiates booking request
-router.post('/bookings',authenticateJWT, createBookingRequest);
-router.get('/bookings/owner',authenticateJWT, getOwnerBookings);
+router.post("/bookings", authenticateJWT, createBookingRequest);
+router.get("/bookings/owner", authenticateJWT, getOwnerBookings);
 router.get("/bookings/owner/all", authenticateJWT, getAllOwnerBookings);
 
-router.get('/bookings/user', authenticateJWT, (req, res, next) => {
-  console.log('GET /bookings/user-bookings hit');
-  next();
-}, getUserBookings);
-router.get('/bookings/:id/invoice', authenticateJWT, downloadInvoice);
+router.get(
+  "/bookings/user",
+  authenticateJWT,
+  (req, res, next) => {
+    console.log("GET /bookings/user-bookings hit");
+    next();
+  },
+  getUserBookings
+);
+router.get("/bookings/:id/invoice", authenticateJWT, downloadInvoice);
 // Owner approves or rejects booking request
-router.post('/bookings/:id/status',authenticateJWT, handleBookingApproval);
+router.post("/bookings/:id/status", authenticateJWT, handleBookingApproval);
 //maintenance request from user
-router.post('/maintenance-request', authenticateJWT, maintenanceRequestHandler);
+router.post("/maintenance-request", authenticateJWT, maintenanceRequestHandler);
 // Get maintenance requests by booking ID
-router.get('/maintenance/requests/owner', authenticateJWT, getRequestsByBookings);
+router.get(
+  "/maintenance/requests/owner",
+  authenticateJWT,
+  getRequestsByBookings
+);
 //update maintenance request status
-router.post('/maintenance/:requestId/update-status', authenticateJWT, updateMaintenanceStatus);
+router.post(
+  "/maintenance/:requestId/update-status",
+  authenticateJWT,
+  updateMaintenanceStatus
+);
 //maintenance history history for user
-router.get('/maintenance/history', authenticateJWT, getMaintenanceHistory);
+router.get("/maintenance/history", authenticateJWT, getMaintenanceHistory);
 //cancel booking request
-router.post('/bookings/:id/cancel', authenticateJWT, cancelBooking);
+router.post("/bookings/:id/cancel", authenticateJWT, cancelBooking);
 
 // User cancels the booking
-router.post('/bookings/:id', cancelBooking);
+router.post("/bookings/:id", cancelBooking);
 // feed back for user
-router.post('/ratings-feedback', authenticateJWT, submitFeedback);
+router.post("/ratings-feedback", authenticateJWT, submitFeedback);
 // Get dashboard stats for the owner
-router.get('/dashboard/owner-stats', authenticateJWT, getOwnerDashboardStats);
+router.get("/dashboard/owner-stats", authenticateJWT, getOwnerDashboardStats);
 //dashboard chart stats
-router.get('/owner/chart-stats', authenticateJWT, getChartStats);
+router.get("/owner/chart-stats", authenticateJWT, getChartStats);
 
 module.exports = router;
