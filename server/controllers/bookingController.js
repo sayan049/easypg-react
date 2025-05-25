1f7e4cb96decb0615ee828693be82acff92d03ab
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-25 18:06:56 +0530

    expire

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1835,25 +1835,4 @@ exports.getChartStats = async (req, res) => {
   }
 };
 
-const startExpirationJob = () => {
-  cron.schedule("*/10 * * * *", async () => {
-    const now = new Date();
-    const expiredThreshold =new Date(now.getTime() - 2 * 60 * 1000);
-    //  new Date(now.getTime() - 24 * 60 * 60 * 1000);
 
-    try {
-      const result = await Booking.updateMany(
-        { status: "pending", createdAt: { $lte: expiredThreshold } },
-        { $set: { status: "expired" } }
-      );
-
-      if (result.modifiedCount > 0) {
-        console.log(`✅ ${result.modifiedCount} pending bookings expired`);
-      }
-    } catch (err) {
-      console.error("❌ Error expiring bookings:", err);
-    }
-  });
-};
-
-module.exports = startExpirationJob;
