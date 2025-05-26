b5dc93e5f5e0d0ae7f7565c778fb14ed20533718
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-26 22:15:35 +0530

    socket

--- server/middleware/is-auth.js
+++ server/middleware/is-auth.js
@@ -6,7 +6,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const authenticateJWT = (req, res, next) => {
  // const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
  const token = req.cookies?.accessToken;
- console.log("axy",token);
+ //console.log("axy",token);
 
   if (!token) {
     return res.status(401).json({ message: "Access token is missing." });
@@ -15,7 +15,7 @@ const authenticateJWT = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded; // Attach user data to the request
-    console.log("Decoded JWT:", decoded);  // Log the decoded token for debugging
+   // console.log("Decoded JWT:", decoded);  // Log the decoded token for debugging
     next();
   } catch (error) {
     console.error("JWT Verification Error:", error);  // Log the error for debugging
