1bd1088211fc9b4caba8b79ab2e54d310c9c05f6
sayan049 <sayanpatra019@gmail.com>
2025-05-27 01:35:10 +0530

    fraud report added

--- client/src/components/ContactOwnerButton.js
+++ client/src/components/ContactOwnerButton.js
@@ -16,16 +16,22 @@ const ContactOwnerButton = ({ mobileNo }) => {
   };
 
   return (
-    <div>
+    <div className="relative inline-block">
+      {/* Tooltip */}
+      {copied && (
+        <div className="absolute -top-10 left-1/2 -translate-x-1/2 bg-green-600 text-white text-xs px-3 py-1 rounded shadow-lg z-10 transition-opacity duration-200 animate-fade-in">
+          <div className="absolute left-1/2 -bottom-1.5 -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-green-600" />
+          Mobile number copied!
+        </div>
+      )}
+
+      {/* Button */}
       <button
         onClick={handleCopy}
-        className="px-4 py-2 text-sm bg-blue-600 text-white rounded-md hover:bg-blue-700"
+        className="px-4 py-2 text-sm bg-blue-600 text-white rounded-md hover:bg-blue-700 relative"
       >
         Contact Owner
       </button>
-      {copied && (
-        <p className="text-green-600 text-sm mt-1">Mobile number copied!</p>
-      )}
     </div>
   );
 };
