783d468889f5f357eab6487e0276341a4c9b88ad
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-26 22:33:19 +0530

    socket

--- client/src/components/dashboardContent.js
+++ client/src/components/dashboardContent.js
@@ -430,7 +430,7 @@ const DashboardContent = ({
                     Contact Owner
                   </button>
                 </div> */}
-                <div className="flex flex-row gap-3  md:w-auto md:items-start flex-wrap">
+                <div className="flex flex-col md:flex-row gap-3  md:w-auto md:items-start">
                   {showCancelInput === stay._id ? (
                     <div className="space-y-2 w-auto">
                       <textarea
