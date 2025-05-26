322101d97c60d524aa95289d4746237ea74bf3a7
myself-Rishi-saha <rishisaha302016@gmail.com>
2025-05-26 22:54:03 +0530

    socket

--- server/index.js
+++ server/index.js
@@ -48,6 +48,7 @@ const io = new Server(server, {
     origin: ORIGIN,
     credentials: true,
   },
+  transports: ["websocket"]
 });
 // const startExpirationJob = require("./cron/expire");
 // startExpirationJob(io);
